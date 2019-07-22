7d9b4f5f85c22bd0897170ffe0bf195a217e366e
meta-yash-sharma <52688921+meta-yash-sharma@users.noreply.github.com>
2019-07-22 16:57:52 +0530

    Update PolynimialTestCase.java

--- Assignment/AbstractAndConcreteDataTypesSCF6/src/Problem2/PolynimialTestCase.java
+++ Assignment/AbstractAndConcreteDataTypesSCF6/src/Problem2/PolynimialTestCase.java
@@ -12,21 +12,23 @@ public class PolynimialTestCase {
 		int [] array2 = {0,7,6};
 		
 		Poly p1 = new Poly(3,array);
-		
-		double ans = p1.evaluate(2);
-		System.out.println(ans);
-		
-		assertEquals(49.0,p1.evaluate(2),0.00);
-		
 		Poly p2 = new Poly(2,array2);
 		
-		int [] addArray = p1.addPoly(p1, p2);
+		//double ans = p1.evaluate(2);
+		//System.out.println(ans);
+		assertEquals(49.0,p1.evaluate(2),0.00);		
 		
+		
+		int [] addArray = {1,9,9,4}; 
+		assertArrayEquals(addArray,p1.addPoly(p1, p2));		
 		for(int i = addArray.length-1; i >= 0 ; i--)
 			System.out.print(addArray[i]+"X^"+i+" ");
 		System.out.println();
 		
-		int [] multiplyPoly = p1.mulPoly(p1, p2);
+		
+		
+		int [] multiplyPoly = {0,7,20,33,46,24};
+		assertArrayEquals(multiplyPoly,p1.mulPoly(p1, p2));
 		for(int i = multiplyPoly.length-1; i >= 0 ; i--)
 			System.out.print(multiplyPoly[i]+"X^"+i+" ");
 	}
