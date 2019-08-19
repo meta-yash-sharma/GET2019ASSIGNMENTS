dec88c60e1f57e06f4c25653e75d67577b8bcf08
meta-yash-sharma <52688921+meta-yash-sharma@users.noreply.github.com>
2019-08-19 14:47:16 +0530

    Update DBMS3_question4.sql

--- Assignment/DBMS3/DBMS3_question4.sql
+++ Assignment/DBMS3/DBMS3_question4.sql
@@ -7,14 +7,13 @@ state VARCHAR(100) NOT NULL,
 PRIMARY KEY(zipcode)
 );
 
-INSERT INTO Zipcode(zipcode, city, state)
-            VALUES(313001, 'udaipur', 'rajasthan'),
-                  (302001, 'jaipur', 'rajasthan'),
-                  (305001, 'ajmer', 'rajasthan'),
-                  (282001, 'agra', 'madhya pradesh'),
-                  (411001, 'pune', 'maharashtra'),
-                  (321001, 'bharatpur', 'rajasthan'),
-                  (400008, 'mumbai', 'maharashtra');
+INSERT INTO Zipcode(zipcode, city, state) VALUES(313001, 'udaipur', 'rajasthan'),
+(302001, 'jaipur', 'rajasthan'),
+(305001, 'ajmer', 'rajasthan'),
+(282001, 'agra', 'madhya pradesh'),
+(411001, 'pune', 'maharashtra'),
+(321001, 'bharatpur', 'rajasthan'),
+(400008, 'mumbai', 'maharashtra');
 
 SELECT zipcode,city,state
 FROM Zipcode
