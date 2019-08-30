abd98fe295c61dbc38d13199a08d7f41cc90e332
meta-yash-sharma <52688921+meta-yash-sharma@users.noreply.github.com>
2019-08-30 15:18:45 +0530

    Update StudentData.java

--- Assignment/EAD_Session3/EAD/src/com/metacube/Servlets/StudentData.java
+++ Assignment/EAD_Session3/EAD/src/com/metacube/Servlets/StudentData.java
@@ -15,44 +15,51 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/StudentData")
 public class StudentData extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public StudentData() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public StudentData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		PrintWriter out = response.getWriter();
-		
+
 		String first_name = request.getParameter("firstname");
-		
+
 		String last_name = request.getParameter("lastname");
-		
+
 		String father_name = request.getParameter("fathername");
-		
+
 		String email = request.getParameter("email");
-		
+
 		int class_no = Integer.parseInt(request.getParameter("class"));
-		
+
 		int age = Integer.parseInt(request.getParameter("age"));
-		
-		
-		StatementOfSQL.insertData(first_name, last_name, father_name, email, class_no, age);
-		
-		out.print("Info of : " + first_name+" "+last_name+" Has been added.");
-		
+
+
+		int a = StatementOfSQL.insertData(first_name, last_name, father_name, email, class_no, age);
 
 		response.setContentType("text/html");
-		out.print("<br><br><h1><a href='index.html'>Visit Home Page.</a></h1>"); 
-		
+
+
+		if(a == 1){
+			out.print("Info of : " + first_name+" "+last_name);
+			out.print("<br><br>");
+			out.print("Student Data Added");
+		} else {
+			out.print("Info of : " + first_name+" "+last_name);
+			out.print("<br><br>");
+			out.print("Student Data Not Added");
+		}
+		out.print("<br><br><h1><a href='index.html'>Visit Home Page.</a></h1>");
 	}
 
 }
