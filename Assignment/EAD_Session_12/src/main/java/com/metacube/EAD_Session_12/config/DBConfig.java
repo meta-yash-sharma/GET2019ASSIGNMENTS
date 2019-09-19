d46bd75d794ef477fe8bdc8c9215bc125bb65c58
meta-yash-sharma <yash.sharma1@metacube.com>
2019-09-19 13:56:21 +0530

    EAD SESSION 12 on Spring And hibernate

---
+++ Assignment/EAD_Session_12/src/main/java/com/metacube/EAD_Session_12/config/DBConfig.java
@@ -0,0 +1,75 @@
+package com.metacube.EAD_Session_12.config;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:dbconfig.properties")
+public class DBConfig {
+
+	@Autowired
+	private Environment env;
+
+	// EntityManager Bean
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+
+	HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	vendorAdapter.setGenerateDdl(true);
+	vendorAdapter.setShowSql(false);
+	vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+	LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+	factory.setJpaVendorAdapter(vendorAdapter);
+	factory.setJpaProperties(hibernateProperties());
+	factory.setPackagesToScan("com.metacube.EAD_Session_12.model");
+	factory.setDataSource(getDataSource());
+	factory.afterPropertiesSet();
+	return factory.getObject();
+	}
+
+	// JpaTransactionManager Bean
+	@Bean
+	public JpaTransactionManager transactionManager() {
+	JpaTransactionManager txManager = new JpaTransactionManager();
+	txManager.setDataSource(getDataSource());
+	txManager.setEntityManagerFactory(entityManagerFactory());
+	return txManager;
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("database.driver"));
+		dataSource.setUrl(env.getProperty("database.url"));
+		dataSource.setUsername(env.getProperty("database.root"));
+		dataSource.setPassword(env.getProperty("database.password"));
+		return dataSource;
+	}
+
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+		properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+		properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+		return properties;
+	}
+}
---
+++ Assignment/EAD_Session_12/src/main/java/com/metacube/EAD_Session_12/repository/StudentRepo.java
@@ -0,0 +1,31 @@
+package com.metacube.EAD_Session_12.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.metacube.EAD_Session_12.model.Student;
+
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
+	
+	
+	 /** No need to define findAll() here, because
+     *  inherited from JpaRepository with many other basic JPA operations.**/
+    //public List<Student> findAll();
+
+    /** spring-jpa-data understands this method name,
+     *  because it supports the resolution of specific keywords inside method names. **/
+	
+	public List<Student> findByfirstNameContainingIgnoreCase(String searchString);
+	
+	
+	/** We can define a JPA query.**/
+    @Query("select s from Student s where s.firstName = :firstName")
+    public List<Student> findByNameIs(@Param("firstName") String name);
+
+}
