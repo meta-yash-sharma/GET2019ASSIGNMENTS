cba9961d768dc39873e9910887429623b8b3ab69
meta-yash-sharma <yash.sharma1@metacube.com>
2019-09-23 19:39:20 +0530

    EAD SESSION 14 on Spring

---
+++ Assignment/EAD_Session_14/src/main/java/com/metacube/EAD_Session_14/service/ServiceClass.java
@@ -0,0 +1,43 @@
+package com.metacube.EAD_Session_14.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.metacube.EAD_Session_14.model.Student;
+import com.metacube.EAD_Session_14.repository.StudentRepo;
+
+
+
+
+
+
+
+@Service
+public class ServiceClass {
+	
+	@Autowired
+	private StudentRepo repo;
+	
+	@Transactional
+    public void add(Student student) {
+		repo.save(student);
+    }
+	
+	@Transactional(readOnly=true)
+    public List<Student> findAll() {
+        return repo.findAll();
+    }
+	
+	@Transactional(readOnly=true)
+    public List<Student> findByNameIs(String name) {
+        return repo.findByNameIs(name);
+    }
+
+    @Transactional(readOnly=true)
+    public List<Student> findByfirstNameContainingIgnoreCase(String searchString) {
+        return repo.findByfirstNameContainingIgnoreCase(searchString);
+    }
+}
---
+++ Assignment/EAD_Session_14/src/test/java/com/metacube/EAD_Session_14/EadSession14ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.metacube.EAD_Session_14;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EadSession14ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
